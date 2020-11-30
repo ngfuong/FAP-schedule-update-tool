c7b03c84a061c9b5a72ee714b69d88239c0f82f0
ngfuong <lenguyenphuong2204@gmail.com>
2020-11-30 11:36:13 +0700

    Hide password

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.support.select import Select
 
 #firefox executable file path
-firefoxBin = FirefoxBinary(r'/usr/local/firefox_dev/firefox')
+firefoxBin = FirefoxBinary(r'/opt/firefox_dev/firefox')
 #add gecko executable file path
 driver = webdriver.Firefox(firefox_binary=firefoxBin, executable_path=r'/usr/local/bin/geckodriver')
 
@@ -25,37 +25,41 @@ driver.implicitly_wait(0.5)
 dropdown = driver.find_element_by_xpath("//*[@name='ctl00$mainContent$ddlCampus']")
 select = Select(dropdown)
 select.select_by_visible_text("FU-Hồ Chí Minh")
-time.sleep(5)
+time.sleep(2)
 
 #CLICK SIGN IN BUTTON
-main_page = driver.current_window_handle
+main_page = driver.window_handles[0]
 button = driver.find_element_by_class_name("g-signin2")
 button.click()  #pop up
-time.sleep(5)
+time.sleep(2)
+print("List of windows:")
+print([window for window in driver.window_handles])
+login_page = driver.window_handles[1]
 
 #CHANGING HANDLE TO SWITCH TO POP UP
-for handle in driver.window_handles: 
-    if handle != main_page: 
-        login_page = handle
+driver.switch_to.window(login_page)
+#for handle in driver.window_handles:
+#    if handle != main_page:
+#        login_page = handle
 
 #LOGIN
-print('Enter email id : ', end ='') 
-email = input().strip() 
-print('Enter password : ', end ='') 
+print('Enter email id...')
+#email = input().strip()
+email = "phuonglnse150214@fpt.edu.vn"
+print('Enter password: ', end ='')
 password = input().strip()
-# enter the email !!!!!!!!!!!!!!!!!!!!!
-driver.find_element_by_xpath("//*[@name='identifier']").send_keys(email)
+driver.find_element_by_id("identifierId").send_keys(email)
 # click the next button
-next = driver.find_element_by_xpath("//*[@id='identifierNext']")
+next = driver.find_element_by_id("identifierNext")
 next.click()
-time.sleep(5)
+time.sleep(2)
 # enter the password
-driver.find_element_by_xpath('//*[@id ="password"]').send_keys(password)
+driver.find_elements_by_name("password")[0].send_keys(password)
 
 # click the login button
-next = driver.find_element_by_xpath('//*[@id="passwordNext"]')
-next.click()
-time.sleep(5)
+login = driver.find_element_by_id("passwordNext")
+login.click()
+time.sleep(2)
 
 #SWITCH TO MAIN PAGE
 driver.switch_to.window(main_page)
