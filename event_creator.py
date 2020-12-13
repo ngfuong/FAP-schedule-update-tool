fc7e0774495b3284eb461de5c8e1bf67a042060e
ngfuong <ngfuong@github.com>
2020-12-13 18:02:18 +0700

    Using the gg calendar api

--- event_creator.py
+++ event_creator.py
@@ -1,13 +1,18 @@
 import json
+import os.path
+import pickle
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 import pprint
 
-timeZone = 'Asia/Bangkok'
 with open('timetable.json', 'r') as inputFile:
-    # Loading data
+    # Loading timetable data
     data = inputFile.read()
     timetable = json.loads(data)
 
-    # Creating event
+    # Reformat event
+    timeZone = 'Asia/Bangkok'
     event_list = []
     for subject in timetable:
         for item in timetable[subject]:
@@ -36,5 +41,19 @@ with open('timetable.json', 'r') as inputFile:
             }
             event_list.append(event)
 
-with open('event.json', 'w') as outputFile:
-    json.dump(event_list, outputFile, ensure_ascii=False, indent=4)
+# USING GOOGLE CALENDAR API TO CREATE EVENT
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+flow = InstalledAppFlow.from_client_secrets_file(
+    'credentials-fap.json', SCOPES
+)
+creds = flow.run_local_server(port=0)
+
+service = build('calendar', 'v3', credentials=creds)
+
+for item in event_list:
+    event = service.events().insert(calendarId='primary', body=item).execute()
+    print("Event created:", event.get('htmlLink'))
+
+#with open('event.json', 'w') as outputFile:
+#    json.dump(event_list, outputFile, ensure_ascii=False, indent=4)
